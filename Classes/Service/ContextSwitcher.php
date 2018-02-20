d78aed67862802a82d1518754633857a6af2b8bd
Denny Lubitz <lubitz@vivomedia.de>
2018-02-20 13:37:19 +0100

    BUGFIX: Use adoptNode of context to get or create node of different dimension

--- Classes/Service/ContextSwitcher.php
+++ Classes/Service/ContextSwitcher.php
@@ -2,12 +2,20 @@
 namespace Ttree\ContentRepositoryImporter\Service;
 
 use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\ContentRepository\Domain\Service\Context;
 use Neos\Eel\FlowQuery\FlowQuery;
+use Neos\Neos\Domain\Service\ContentContextFactory;
 use Ttree\ContentRepositoryImporter\Domain\Model\ProviderPropertyValidity;
 use Neos\Flow\Annotations as Flow;
 
 class ContextSwitcher
 {
+    /**
+     * @Flow\Inject
+     * @var ContentContextFactory
+     */
+    protected $contextFactory;
+
     /**
      * @var NodeInterface
      */
@@ -20,15 +28,27 @@ class ContextSwitcher
 
     /**
      * @param array $dimensions
+     * @param string $workspaceName
      * @return NodeInterface|null
      */
-    public function to(array $dimensions)
+    public function to(array $dimensions, $workspaceName = 'live')
     {
-        return (new FlowQuery([$this->node]))->context([
-            'dimensions' => $dimensions,
-            'targetDimensions' => array_map(function ($dimensionValues) {
-                return array_shift($dimensionValues);
-            }, $dimensions)
-        ])->get(0);
+        $context = $this->createContext($dimensions, $workspaceName);
+        return $context->adoptNode($this->node);
+    }
+
+    /**
+     * Create a context with the given workspace and dimensions
+     *
+     * @param array $dimensions
+     * @param string $workspaceName
+     * @return Context
+     */
+    protected function createContext(array $dimensions, $workspaceName)
+    {
+        return $this->contextFactory->create(array(
+            'workspaceName' => $workspaceName,
+            'dimensions' => $dimensions
+        ));
     }
 }
